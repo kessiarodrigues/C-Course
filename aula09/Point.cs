8d0e2b0355e2e3b6a8d5bc9b75faa0bb7a4373b9
kessia <kessia.rodrigues@novahaus.com.br>
2023-02-28 15:55:57 -0300

    teste

---
+++ aula09/Point.cs
@@ -0,0 +1,11 @@
+namespace aula09 {
+    struct Point{
+        public double X;
+        public double Y;
+
+        public override string ToString()
+        {
+            return "(" + X +  "," + Y + ")" ;
+        }
+    }
+}
