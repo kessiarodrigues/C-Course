2e040884cf5ab1e8f3f0d09038be5300da74843b
kessia <kessia.rodrigues@novahaus.com.br>
2023-03-06 16:26:19 -0300

    .

---
+++ aula18/Entities/SavingsAccount.cs
@@ -0,0 +1,22 @@
+using aula18.Entities;
+
+namespace aula18
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount(){
+
+        }
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+    }
+}
